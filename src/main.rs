c969bb33bc2c860b9fc220e44e3415a6cf964114
Alam Sanchez <asancheztorres1905@gmail.com>
2022-09-29 23:10:26 -0500

    Remove duplicate grammar

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ pub mod ast;
 
 #[test]
 fn grammar() {
-    let target = "(256 * (55 - 3 / 4) + 326 - 54) > 34 && 54 > 57 && (33 / 32 - 34) * 17 > 544 - 2 ";
+    let target = "(26 * (55 - 3 / 4) + 326 - 54) > 34 && 54 > 57 && (33 / 32 - 34) * 17 > 544 - 2 ";
 
     let expr = grammar::ExprParser::new()
         .parse(&target)
